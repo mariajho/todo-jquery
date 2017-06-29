763d203f8b3f57190fa2907c5b0c28c1b02340ed
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-06-28 20:13:50 -0400

    avanzando-jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
-$(document).ready(function){
+$(document).ready(function(){
 	$("button").click(function(e){
 		e.preventDefault();
-		var name = $("#nombre").val();
-		var message = $("#mensaje").val();
-		if(name == "" &&message == ""){
-			alert("Debes escribir tu nombre y el mensaje");
-		}else{
-			$('#comentarios').append('<div><span class="center-align">' + name + '</span><p class=center-align">' + message + '</p></div>');
+		var datos = $("#tarea").val();
+		if(datos == ""){
+			alert("Ingresa una tarea");	
+		}
+		else{
+			$("#completar").append("<div><span class='center-align'>"+'<br>' + datos + '<br>' + '<button>Remove</button>');	
 		}
 	});
 });
